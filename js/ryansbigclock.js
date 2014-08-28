5efe10458d411fd81e73f3a6588fa57c88a07a08
Ryan Meyers <rjmcw@live.com>
2014-08-28 16:42:34 -0500

    Use localStorage for persistant calendar id

--- js/ryansbigclock.js
+++ js/ryansbigclock.js
@@ -14,14 +14,29 @@ $(window).load(function() {
 		var myTimeInterval=setInterval(function(){myTimer()},1000);
         var myTimeInterval2=setInterval(function(){myTimer2()},131);
         
+
+        if(localStorage.getItem("googleCalendarId") === null){
+            localStorage.setItem("googleCalendarId", googleCalendarId);
+        } else {
+            googleCalendarId = localStorage.getItem("googleCalendarId");
+        }
+
+
         loadTumbo();
  		
         
  		var myTimeInterval=setInterval(function(){myTimer()},1000);
 		var myTimeInterval2=setInterval(function(){myTimer2()},131);
         
- 		
 
+
+        $('#gcal-address').val(googleCalendarId);
+ 		
+        $('#gcal-address-check').click(function(){
+            googleCalendarId = $('#gcal-address').val();
+            localStorage.setItem("googleCalendarId", googleCalendarId);
+            loadTumbo();
+        });
  	},
     afterResize: function(){
         showTumbo(null);
