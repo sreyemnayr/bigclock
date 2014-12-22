5b635a862f626a348447cb4203be5a27b580cac4
sreyemnayr <rjmcw@live.com>
2014-12-22 16:16:31 -0600

    lock and other changes
    ability to lock

--- html/js/ryansbigclock.js
+++ html/js/ryansbigclock.js
@@ -1,6 +1,75 @@
 var scrollUnlocked = true;
+var keys = {};
+
+var debounced = $.debounce( 1200, true, showLock );
+
+function checkKeys(e,down) {
+  down = typeof down === 'boolean' ? down : false;
+  if(down)  
+  {
+    keys[e.which] = true;
+     if(keys.hasOwnProperty("32") && keys.hasOwnProperty("75") && keys.hasOwnProperty("76"))
+        return true;
+    else
+      return false; 
+  }
+    else 
+    {
+        delete keys[e.which];
+        return false;
+  
+    }
+
+    return false;
+  
+   
+}
+
+function setLock(u){
+  u = typeof u === 'boolean' ?  u : false;
+
+  if(u)
+  {
+        $.fn.fullpage.setAllowScrolling(false);
+        $.fn.fullpage.setKeyboardScrolling(false); 
+        $('#buttonLock').empty().append('<i class="fa fa-fw fa-lock"> </i>'); 
+        scrollUnlocked = false;
+  }
+  else
+  {
+         $.fn.fullpage.setAllowScrolling(true);
+         $.fn.fullpage.setKeyboardScrolling(true);  
+         $('#buttonLock').empty().append('<i class="fa fa-fw fa-unlock"> </i>');
+         scrollUnlocked = true;
+   }
+
+}
+function unlock(){
+  setLock(false);
+}
+function lock(){
+  setLock(true);
+}
+
+function showLock(){
+ 
+ if(scrollUnlocked === false){
+
+    $.gritter.add({
+                    
+                    text: '<i class="fa fa-fw fa-lock"> </i>',
+                    time: 1000,
+                    position: 'bottom-right'
+                });
+  }
+}
+
 
 $(window).load(function() {
+
+
+ 
+
     $('#footer').fadeTo(200,0.05);
 
  $('#fullpage').fullpage({
@@ -23,8 +92,26 @@ $(window).load(function() {
         } else {
             googleCalendarId = localStorage.getItem("googleCalendarId");
         }
+
+         if(localStorage.getItem("startLocked") === null){
+            localStorage.setItem("startLocked", startLocked);
+        } else {
+            startLocked = JSON.parse(localStorage.getItem("startLocked"));
+        }
+
+        //console.log(startLocked);
         
         loadTumbo();
+
+        if(startLocked)
+        {
+            document.getElementById('startLocked').checked = true;
+            lock();
+        }
+        else
+        {
+            document.getElementById('startLocked').checked = false;
+        }
  		
         
  		var myTimeInterval=setInterval(function(){myTimer()},1000);
@@ -36,6 +123,28 @@ $(window).load(function() {
             googleCalendarId = $('#gcal-address').val();
             localStorage.setItem("googleCalendarId", googleCalendarId);
             loadTumbo();
+        });
+
+        $(document).keydown(function (e) {
+           if(!scrollUnlocked) 
+            {
+              e.preventDefault();
+             
+             if(checkKeys(e,true)) 
+              {
+                unlock();
+              }
+             
+            }
+        });
+
+        $(document).keyup(function (e) {
+           checkKeys(e,false);
+
+           if(!scrollUnlocked)
+           {
+              debounced();
+           }
         });
 
          $('#gcal-address').keypress(function(){
@@ -81,35 +190,33 @@ $(window).load(function() {
         $('#buttonLock').click(function(){
             if(scrollUnlocked === true)
             {
-                $.fn.fullpage.setAllowScrolling(false);
-                $.fn.fullpage.setKeyboardScrolling(false); 
-                 $('#buttonLock').empty().append('<i class="fa fa-fw fa-lock"> </i>'); 
-                scrollUnlocked = false;
+               lock();
             }
             else
             {
-                 $.fn.fullpage.setAllowScrolling(true);
-                $.fn.fullpage.setKeyboardScrolling(true);  
-                 $('#buttonLock').empty().append('<i class="fa fa-fw fa-unlock"> </i>');
-                scrollUnlocked = true;
-            }
+            // unlock();
+             //  $.throttle(1000, showLock);
+              showLock();
+             }
 
         });
+
+        $(document).bind('contextmenu', function (e) {
+            if(scrollUnlocked === true)
+            {
+             var dummy = 0;
+            }
+            else
+            {
+              e.preventDefault();
+              showLock();
+            }
+          });
 
         $(document).bind('keydown', 'l', function(){
             if(scrollUnlocked === true)
             {
-                $.fn.fullpage.setAllowScrolling(false);
-                $.fn.fullpage.setKeyboardScrolling(false);  
-                 $('#buttonLock').empty().append('<i class="fa fa-fw fa-lock"> </i>');
-                scrollUnlocked = false;
-            }
-            else
-            {
-                 $.fn.fullpage.setAllowScrolling(true);
-                $.fn.fullpage.setKeyboardScrolling(true);  
-                 $('#buttonLock').empty().append('<i class="fa fa-fw fa-unlock"> </i>');
-                scrollUnlocked = true;
+                 lock();
             }
           
 
@@ -122,8 +229,13 @@ $(window).load(function() {
             $(this).fadeTo(500,0.05);
         });
 
+        $('#startLocked').change(function(){
+             localStorage.setItem("startLocked", $(this).is(":checked"));
+             console.log($(this).is(":checked"));
+        });
 
-
+        $.extend($.gritter.options, { 
+        position: 'bottom-right'});
 
  	},
     afterResize: function(){
